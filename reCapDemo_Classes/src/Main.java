b94d7aef23354faf34e08190461bf3282251dd14
Sertan Ortaoğlan <154704946+SertanOrtaoglan@users.noreply.github.com>
2024-11-26 22:51:52 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();    //class'ların en temel özelliği ortak operasyonları tutmasıdır.(Bu örnekte görüldüğü gibi)
+        int sonuc = dortIslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
+
