2ffdc0bf3e3ae171f5029d8d2807fb782ee0315e
Sertan Ortaoğlan <154704946+SertanOrtaoglan@users.noreply.github.com>
2024-11-26 16:07:28 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12; //reusability: Tekrar kullanılabilirlik demektir. Bir kere yaz onu her yerde kullan aynı şeyi defalarca yazma!
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+    }
+}
