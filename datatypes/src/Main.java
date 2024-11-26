cf68492e08162c261c61ce061ccfd11bf61d5996
Sertan Ortaoğlan <154704946+SertanOrtaoglan@users.noreply.github.com>
2024-11-26 16:32:36 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 12; //java'da tam sayıları tutan default ifade 'int(integer)'dır.
+        sayi = 1311111111;
+
+        double sayi1 = 12.5; //java'da ondalıklı sayıları tutan default ifade 'double'dır.
+
+        char karakter = 'A'; //'char' veri tipi sadece tek karakter tutar.('' tek tırnak içinde!)
+
+        String sehir = "ANKARA";
+
+        boolean dogruMu = false; //'boolean' veri tipi 'true' ya da 'false'u tutar. (1 veya 0)
+    }
+}
